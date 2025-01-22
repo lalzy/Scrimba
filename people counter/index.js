4618c5012e62a0e4b035fb9c2c7619c11997391b
skn <svenyk@gmail.com>
2025-01-22 13:15:47 +0100

    Add files via upload

---
+++ people counter/index.js
@@ -0,0 +1,25 @@
+var countEl = document.getElementById("count-el");    
+let count = 0
+let saved = false
+function increment(){
+   count = parseInt(countEl.textContent)
+    countEl.textContent = ++count;
+}
+
+function resetCount(){
+    count = 0
+    countEl.textContent = count;
+}
+
+function save(){
+    let doc = document.getElementById("save-el");
+    if(!doc.textContent.includes(count) && count = 0){
+        if(saved){
+            doc.textContent += ` - ${count}`
+        }else{
+            saved = true
+            doc.textContent += `${count}`
+        }
+    }
+    resetCount()
+}
