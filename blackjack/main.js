f50ed364edc517d5f23b7b58f08dad3bd72f8d1b
skn <svenyk@gmail.com>
2025-01-23 14:55:48 +0100

    kuhhh

--- blackjack/main.js
+++ blackjack/main.js
@@ -1,67 +1,86 @@
-
-// cards.sort((a, b)=>{return Math.random()})
-
-const suit = 0
-const value = 1
-
-// Todo: Create deck
-// TODO: Create shuffle
-
-function initialize (){
-    CreateDeck()
-    playerHand = []
-    dealerHand = []
-}
-
-
-var playerHand = []
-var dealerhand = []
-
-function draw(amount, array){
-    for(let i = 0; i < amount; i++){
-        array.push(cards.pop())
-    }
-
-    return array;
-}
-
-let won = false
-
-function sumUpCards(array){
-    let sum = 0
-    array.forEach(element => {
-        if(element[value] == 11){
-            if ((sum + element[value]) > 21){
-                sum += 1
-            }else{
-                won = true
-                return -1
-            }
-        }else if(element[value] > 10){
-            sum += 10
-        }else{
-            sum += element[value]
-        }
-    });
-
-    return sum;
-}
-
 let playerSum = 0
 let dealerSum = 0
-
+var hit21 = false;
+var bust = false;
 function display(){
     document.getElementById("playerSum").innerText = playerSum;
+    document.getElementById("dealerSum").innerText = dealerSum;
 }
 
-function start (){
-    initialize();
-    playerHand = draw(2, playerHand);
-    dealerHand = draw(2, dealerhand);
+/**Rewrite to draw cards and sum up instead. */
+function draw(amount){
+    let sum = 0
+    for (let i = 0; i < amount ; i++){
+        let val = Math.floor(Math.random() * 13) +1
+        console.log("card = " + val)
+        if(val == 11){
+            if ((sum + 10) > 21){
+                sum += 1
+            }else{
+                sum += 10
+            }
+        }else if (val > 10){
+            sum += 10
+        }else{
+            sum = val
+        }
+    }
+    return sum
+}
+function init(){
+    document.getElementById("wonmessage").style.display = "none"
+    document.getElementById("dealerwonmessage").style.display = "none"
+}
 
-    playerSum = sumUpCards(playerHand)
-    dealerSum = sumUpCards(dealerhand)
+function start(){
+    init()
 
+    dealerSum = 0;
+    playerSum = draw(2);
     display()
+}
 
+function lose(){
+    document.getElementById("dealerwonmessage").style.display = "block"
+}
+
+function won(){
+    document.getElementById("wonmessage").style.display = "block"
+}
+
+function hit(){
+    playerSum += draw(1);
+    if(playerSum > 21){
+        bust = true
+       /* deactivate hit */ 
+    }else if (playerSum == 21){
+        hit21 = true
+    }
+    display()
+}
+
+function winCheck (){
+    if(dealerSum > 21){
+        won()
+    }else if(dealerSum == 21){
+        lose()
+    }else if(dealerSum > playerSum){
+        lose()
+    }
+}
+
+function stand(){
+    if(hit21){
+        won()
+    }else if(bust){
+        lose()
+    }else {
+        console.log("---dealer draw:---")
+        dealerSum = draw(2);
+        while(dealerSum < 21 && dealerSum < playerSum){
+            dealerSum += draw(1)
+        }
+        winCheck()
+    }
+    display()
 }
